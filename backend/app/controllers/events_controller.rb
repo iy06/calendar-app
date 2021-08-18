01e9d0e61ad6863a39f65ed46b5cbb8db7bdb076
iy06 <yuuki9916@gmail.com>
2021-08-18 14:57:18 +0900

    showとcreateを定義

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -3,4 +3,33 @@ class EventsController < ApplicationController
     # イベントデータの一覧を返す
     render json: Event.all
   end
+
+  def show
+    # 指定したイベントデータを返す
+    render json: Event.find(params[:id])
+  end
+
+  def create
+    # パラメーターの値でイベントデータを作成する
+    event = Event.new(event_params)
+    if event.save
+      render json: event
+    else
+      render json: event.errors, status: 422
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :id,
+      :name,
+      :start,
+      :end,
+      :timed,
+      :description,
+      :color
+    )
+  end
 end
